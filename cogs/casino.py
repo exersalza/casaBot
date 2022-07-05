a89b0fd2c1b416dba15b96d49a548b00cf1f01e5
exersalza <squidodle44@gmail.com>
2022-07-05 18:20:15 +0200

    finished casion

--- cogs/casino.py
+++ cogs/casino.py
@@ -1,5 +1,7 @@
-from nextcord.ext import commands
+from random import randint
 from typing import TYPE_CHECKING
+
+from nextcord.ext import commands
 
 if TYPE_CHECKING:
     from main import Main
@@ -7,7 +9,33 @@ if TYPE_CHECKING:
 
 class Casino(commands.Cog):
     def __init__(self, bot: 'Main'):
-        self.bot = bot
+        self.bot: Main = bot
+
+    @commands.command()
+    async def gamble(self, ctx, bet: int = 100):
+        user_id = ctx.author.id
+
+        try:
+            user_coins = self.bot.conn.get('SELECT points.coins FROM dcbots.points '
+                                           'WHERE points.user = %s', (user_id,))[0][0]
+        except IndexError as e:
+            await ctx.send('Es ist ein Fehler aufgetreten, versuche es später erneut.')
+            print(e)
+            return
+        print(user_coins)
+
+        if bet > user_coins:
+            await ctx.send('Du hast nicht genug Coins.')
+            return
+
+        if bet < 0:
+            await ctx.send('Du kannst nicht negativ oder null werden.')
+            return
+
+        if randint(0, 1):
+            await ctx.send('Du hast gewonnen!')
+        else:
+            await ctx.send('Du hast verloren!')
 
 
 def setup(bot) -> int:
